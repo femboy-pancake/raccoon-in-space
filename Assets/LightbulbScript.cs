ebde05d7eb3450c126a1c6cd176199acc1b2b790
Pancake <sspel25adni@axevalla.fhsk.se>
2026-02-27 11:52:21 +0100

    level 3

---
+++ Assets/LightbulbScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LightbulbScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene("level 2");
+
+        }
+
+
+    }
+}
