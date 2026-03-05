e37e974f559fae3d4827300afc86ab88c83ef3db
Pancake <sspel25adni@axevalla.fhsk.se>
2026-03-05 11:51:07 +0100

    need to do rotation tomorrow

--- Assets/ladderTeleporterScript.cs
+++ Assets/ladderTeleporterScript.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 public class ladderTeleporterScript : MonoBehaviour
 {
+
+    public Light2D EnableLight;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene("level 3");
+            EnableLight = other.GetComponent<Light2D>();
+            SceneManager.LoadScene("lighttest");
+            EnableLight.enabled = true;
 
-            other.transform.position = new Vector3(12.28474f, -4.4f, 0);
+            other.transform.position = new Vector3(12.34f, -4.42f, 9);
+
+
             // Thread.Sleep(TimeSpan.FromSeconds(0.5));
         }
 
