e87d0c002045b05127eacb8cef005201f027d5eb
Pancake <sspel25adni@axevalla.fhsk.se>
2026-04-13 16:08:19 +0200

    ekoed

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -26,13 +26,21 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        player = GameObject.Find("Player (2)");
+       /* player = GameObject.Find("Player (2)");
         graphics = GameObject.Find("graphics");
-
+        playerSprite  = graphics.GetComponent<Transform>();
         spawnPosition = GameObject.Find("SpawnPosition").transform;
+        playerjumpscript = player.GetComponent<playerJumpScript>();
+        playerscript = player.GetComponent<PlayerScript>();
+        ridgidbody = player.GetComponent<Rigidbody2D>();
+        PlayerRenderer = player.GetComponent<SpriteRenderer>();
+        DestroyableWall = GameObject.FindGameObjectsWithTag("DestoryableWall"); */
+
+
     }
     public void RestartLevel()
     {
+        DestroyableWall = GameObject.FindGameObjectsWithTag("DestroyableWall");
         player.transform.position = spawnPosition.position;
         player.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
         Physics2D.gravity = new Vector2(0, -9.8f);
@@ -42,23 +50,31 @@ public class GameManager : MonoBehaviour
         playerscript.isVerticalright = false;
         playerscript.isUpsidedown = false;
         ridgidbody.linearVelocity = Vector3.zero;
-        collectobject.SetActive(true);
-        foreach (GameObject wall in DestroyableWall)
+
+        if(collectobject != null)
+            collectobject.SetActive(true);
+
+        if (DestroyableWall != null)
         {
-            wall.GetComponent<BoxCollider2D>().enabled = true;
-            wall.GetComponent<SpriteRenderer>().enabled = true;
+            foreach (GameObject wall in DestroyableWall)
+            {
+                wall.GetComponent<BoxCollider2D>().enabled = true;
+                wall.GetComponent<SpriteRenderer>().enabled = true;
+            }
         }
     }
 
     private void Awake()
     {
-        // public Transform playerSprite;
-        /*player = GameObject.Find("Player (2)");
+
+        player = GameObject.Find("Player (2)");
         graphics = GameObject.Find("graphics");
+        playerSprite = graphics.GetComponent<Transform>();
+        spawnPosition = GameObject.Find("SpawnPosition").transform;
         playerjumpscript = player.GetComponent<playerJumpScript>();
         playerscript = player.GetComponent<PlayerScript>();
         ridgidbody = player.GetComponent<Rigidbody2D>();
-        PlayerRenderer = graphics.GetComponent<SpriteRenderer>();*/
-
+        PlayerRenderer = player.GetComponent<SpriteRenderer>();
+        DestroyableWall = GameObject.FindGameObjectsWithTag("DestroyableWall");
     }
 }
